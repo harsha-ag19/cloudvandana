024febad76b808361703d7572f0adea530e236f6
harsha-ag19 <88489050+harsha-ag19@users.noreply.github.com>
2023-10-21 22:08:02 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,20 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+            console.log(e.target); 
+      }
+  })
+})
